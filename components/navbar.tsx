d8c7a7fd849bce8c6f7dac8586ae82e6635463ad
junkyeom <junkyeom0203@naver.com>
2025-01-21 21:10:53 +0900

    nav 기초 구조 완성

---
+++ components/navbar.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Navbar() {
+    return (
+        <div className="flex bg-transparent w-full items-center px-5 py-4">
+            <div className="w-[160px] h-[64px] relative mr-10">
+                <Link href="/"><Image src="/logo.png" alt="couchilla" fill></Image></Link>
+            </div>
+            <div className="text-custom-color font-[Anton] text-xl">
+                <Link href="/" className="mr-5">LP/CD</Link>
+                <Link href="/" className="mr-5">GOODS</Link>
+                <Link href="/" className="mr-5">EVENT</Link>
+            </div>
+            <div className="text-custom-color font-[Anton] text-xl ml-auto">
+                <Link href="/" className="mr-5">LOGIN</Link>
+            </div>
+        </div>
+    )
+}
