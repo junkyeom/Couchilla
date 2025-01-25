d85d745ae89040ea3ab288c872bf160fb4208165
박준겸 <junkyeom0203@naver.com>
2025-01-26 02:23:32 +0900

    계절별 캐러셀 디자인 완

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
-import Carousel from "@/components/carousel";
+'use client'
+
+import SeasonCarousel from "@/components/carousels/season-carousel";
+import Carousel from "@/components/main-carousel";
 
 export default function Home() {
   return (
@@ -6,22 +9,27 @@ export default function Home() {
       <div>
         <Carousel/>
       </div>
-      <div>
-        <div className="m-8 mt-32">
-          <p className="text-2xl text-white font-anton">
-            Winter Vibes: Cozy R&B
-          </p>
-          <p className="text-3xl text-custom-pink font-bold font-noto mt-2">
-            추운 겨울, 따뜻한 R&B
-          </p>
-        </div>
-        <div className="m-8 mt-32">
-          <p className="text-2xl text-white font-anton">
-            Explore by Genre
-          </p>
-          <p className="text-3xl text-custom-pink font-bold font-noto mt-2">
-            장르별 모아보기
-          </p>
+      <div className="flex justify-center">
+        <div className="w-[85%]">
+          <div className="m-8 mt-32">
+            <p className="text-2xl text-white font-anton">
+              Winter Vibes: Cozy R&B
+            </p>
+            <p className="text-3xl text-custom-pink font-bold font-noto mt-2">
+              추운 겨울, 따뜻한 R&B
+            </p>
+            <div className="my-8">
+              <SeasonCarousel/>
+            </div>
+          </div>
+          <div className="m-8 mt-32">
+            <p className="text-2xl text-white font-anton">
+              Explore by Genre
+            </p>
+            <p className="text-3xl text-custom-pink font-bold font-noto mt-2">
+              장르별 모아보기
+            </p>
+          </div>
         </div>
       </div>
     </div>
---
+++ components/carousels/season-carousel.tsx
@@ -0,0 +1,120 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import Image from "next/image";
+
+export default function SeasonCarousel() {
+  return (
+    <Swiper
+      modules={[Navigation, Pagination, Autoplay]}
+      spaceBetween={60}
+      slidesPerView={4} 
+      navigation 
+      loop={true} 
+      autoplay={{ delay: 3000, disableOnInteraction: false }}
+    >
+      <SwiperSlide>
+        <div className="aspect-[1/1]" >
+            <div className="w-full h-full relative">
+                <Image src="/thumb/blonde.png" alt="blonde" fill/>
+            </div>
+            <div className="text-center mt-4 font-noto">
+                <p className="text-white text-xl line-clamp-2">
+                    Frank Ocean - Blonde (2023)
+                </p>
+                <div className="mt-4">
+                    <p className="text-white text-lg">
+                        구매가
+                    </p>
+                    <p className="text-white text-lg">
+                        250,000원
+                    </p>
+                </div>
+            </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+      <div className="aspect-[1/1]" >
+            <div className="w-full h-full relative">
+                <Image src="/thumb/sos.png" alt="blonde" fill/>
+            </div>
+            <div className="text-center mt-4 font-noto">
+                <p className="text-white text-xl line-clamp-2">
+                    SZA - SOS
+                </p>
+                <div className="mt-4">
+                    <p className="text-white text-lg">
+                        구매가
+                    </p>
+                    <p className="text-white text-lg">
+                        320,000원
+                    </p>
+                </div>
+            </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+      <div className="aspect-[1/1]" >
+            <div className="w-full h-full relative">
+                <Image src="/thumb/freudian.png" alt="blonde" fill/>
+            </div>
+            <div className="text-center mt-4 font-noto">
+                <p className="text-white text-xl line-clamp-2">
+                    Daniel Caesar - Fredian (Blue)
+                </p>
+                <div className="mt-4">
+                    <p className="text-white ">
+                        구매가
+                    </p>
+                    <p className="text-white text-lg">
+                        117,200원
+                    </p>
+                </div>
+            </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+      <div className="aspect-[1/1]" >
+            <div className="w-full h-full relative">
+                <Image src="/thumb/gabriel.png" alt="blonde" fill/>
+            </div>
+            <div className="text-center mt-4 font-noto">
+                <p className="text-white text-xl line-clamp-2">
+                    Keshi - Gabriel (Red Translucent)
+                </p>
+                <div className="mt-4">
+                    <p className="text-white text-lg">
+                        구매가
+                    </p>
+                    <p className="text-white text-lg">
+                        55,000원
+                    </p>
+                </div>
+            </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+      <div className="aspect-[1/1]" >
+            <div className="w-full h-full relative">
+                <Image src="/thumb/orange.png" alt="blonde" fill/>
+            </div>
+            <div className="text-center mt-4 font-noto">
+                <p className="text-white text-xl line-clamp-2">
+                    Emotional Oranges - The Juice Vol. 2
+                </p>
+                <div className="mt-4">
+                    <p className="text-white text-lg">
+                        구매가
+                    </p>
+                    <p className="text-white text-lg">
+                        60,000원
+                    </p>
+                </div>
+            </div>
+        </div>
+      </SwiperSlide>
+    </Swiper>
+  );
+}
